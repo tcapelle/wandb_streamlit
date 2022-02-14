d247bffc1395fdd370a3fbf95693c176a53a7348
Thomas Capelle <tcapelle@pm.me>
2022-02-14 19:00:05 +0100

    working version 1.0

--- app.py
+++ app.py
@@ -1,4 +1,57 @@
+import os
+import wandb
 import streamlit as st
+import streamlit.components.v1 as components 
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+from utils import train
+
+project = "st"
+entity = "capecape"
+
+HEIGHT = 720
+
+def get_project(api, name, entity=None):
+    return api.project(name, entity=entity).to_html(height=HEIGHT)
+
+st.title("Let's log some metrics to wandb 👇")
+
+# Sidebar
+sb = st.sidebar
+sb.title("Train your model")
+# wandb_token = sb.text_input("paste your wandb Api key if you want: https://wandb.ai/authorize", type="password")
+
+
+# wandb.login(key=wandb_token)
+wandb.login(anonymous="allow")
+api = wandb.Api()
+
+# render wandb dashboard
+components.html(get_project(api, project, entity), height=HEIGHT)
+
+# run params
+runs = sb.number_input('Number of runs:', min_value=1, max_value=10, value=1)
+epochs = sb.number_input('Number of epochs:', min_value=1, max_value=1000, value=100)
+
+
+
+pseudo_code = """
+We will execute a simple training loop
+```python
+wandb.init(project="st", ...)
+for i in range(epochs):
+acc = 1 - 2 ** -i - random()
+loss = 2 ** -i + random()
+wandb.log({"acc": acc, 
+            "loss": loss})
+```
+"""
+
+sb.write(pseudo_code)
+
+# train model
+if sb.button("Run Example"):
+    my_bar = sb.progress(0)
+    print("Running training")
+    for i in range(runs):
+        train(project=project, entity=entity, epochs=epochs)
+        my_bar.progress((i+1)/runs)
---
+++ utils.py
@@ -0,0 +1,29 @@
+import random, time
+
+import wandb
+
+
+def train(project="st", entity=None, epochs=10):
+    run = wandb.init(
+        # Set the project where this run will be logged
+        project=project, 
+        entity=entity,
+        # Track hyperparameters and run metadata
+        config={
+        "learning_rate": 0.02,
+        "architecture": "CNN",
+        "dataset": "CIFAR-100",
+        "epochs": epochs,
+        })
+
+    # This simple block simulates a training loop logging metrics
+    offset = random.random() / 5
+    for epoch in range(1, epochs+1):
+        acc = 1 - 2 ** -epoch - random.random() / epoch - offset
+        loss = 2 ** -epoch + random.random() / epoch + offset
+        # 2️⃣ Log metrics from your script to W&B
+        wandb.log({"acc": acc, "loss": loss})
+        time.sleep(0.1)
+        
+    # Mark the run as finished
+    wandb.finish()
